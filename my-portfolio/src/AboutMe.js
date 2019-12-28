bdad3d0e8585e86e25963cbb20c228d7d6afabf2
unknown <raymondlevengood@gmail.com>
2019-12-27 20:58:36 -0500

    added header/footer/component skeleton

---
+++ my-portfolio/src/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AboutMe.css';
+
+function AboutMe() {
+  return (
+      <div className='body'>
+        <p>KEKdsmlkgsdg
+        ef ka vjkbfkjvkjvbkjvbjskvb</p>
+      </div>
+  )
+}
+
+  export default AboutMe;
--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AboutMe from './AboutMe.js'
+
+//want scroll functionality
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='flexContainer'>
+      <header className='header'>
+        <h1>Hello World. Welcome to Ray's new and improved portfolio.</h1>
+        <nav className='navigation'>Navigation links go here</nav>
       </header>
+      <div className='carosoule'>
+       <p>carosoule stuff here</p>
+      </div>
+      <body>
+        <AboutMe className='aboutMe' />
+      </body>
+      <footer className='footer'>
+        <p>Copyright Info. Quick Links. Email/Twitter Button/Etc</p>
+      </footer>
     </div>
   );
 }
